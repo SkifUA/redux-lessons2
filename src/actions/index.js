7c4d0b5ba455b3e436c1023e7d409a079c8c2045
Valerii Kuzin <kuzin.valeriy@gmail.com>
2017-09-17 14:23:29 +0300

    Added conditional rendering

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,6 @@
 export function selectBook(book) {
   // select book is an ActionCreator, it needs to return an action,
   // an object with a type property.
-
-  console.log('A book has been selected', book.title)
   return {
     type: 'BOOK_SELECTED',
     payload: book
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: 'JavaScript: The Good Parts' },
-    { title: 'Harry Potter' },
-    { title: 'The DArk Tower' },
-    { title:  'Eloquent Ruby' }
+    { title: 'JavaScript: The Good Parts', pages: 101 },
+    { title: 'Harry Potter', pages: 88 },
+    { title: 'The Dark Tower', pages: 53 },
+    { title:  'Eloquent Ruby', pages: 112 }
   ];
 }
